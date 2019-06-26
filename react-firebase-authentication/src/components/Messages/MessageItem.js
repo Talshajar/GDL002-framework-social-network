38b729e8ee8baec690f44da8ce23cb87787f009d
Talshajar <rocio.chavoya@gmail.com>
2019-06-26 10:40:33 -0500

    se agregan las funciones createpost, editpost, removepost

---
+++ react-firebase-authentication/src/components/Messages/MessageItem.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+
+class MessageItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editMode: false,
+      editText: this.props.message.text,
+    };
+  }
+
+  onToggleEditMode = () => {
+    this.setState(state => ({
+      editMode: !state.editMode,
+      editText: this.props.message.text,
+    }));
+  };
+
+  onChangeEditText = event => {
+    this.setState({ editText: event.target.value });
+  };
+
+  onSaveEditText = () => {
+    this.props.onEditMessage(this.props.message, this.state.editText);
+
+    this.setState({ editMode: false });
+  };
+
+  render() {
+    const { authUser, message, onRemoveMessage } = this.props;
+    const { editMode, editText } = this.state;
+
+    return (
+      <li className="container">
+        {editMode ? (
+          <input
+            type="text"
+            value={editText}
+            onChange={this.onChangeEditText}
+            className="inner"
+          />
+        ) : (
+          <span>
+            <strong>{message.userId}</strong> {message.text}
+            {message.editedAt && <span>(Edited)</span>}
+          </span>
+        )}
+
+        {authUser.username === message.userId && (
+          <span>
+            {editMode ? (
+              <span>
+                <button onClick={this.onSaveEditText}>Save</button>
+                <button onClick={this.onToggleEditMode}>Reset</button>
+              </span>
+            ) : (
+              <button onClick={this.onToggleEditMode}>Edit</button>
+            )}
+
+            {!editMode && (
+              <button
+                type="button"
+                onClick={() => onRemoveMessage(message.username)}
+              >
+                Delete
+              </button>
+            )}
+          </span>
+        )}
+      </li>
+    );
+  }
+}
+
+export default MessageItem;
